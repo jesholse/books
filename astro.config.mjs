ff93e729b3560e137abe72cad194a572f2db45fa
jeshol <jesholmlund@gmail.com>
2023-09-26 12:13:47 +0200

    adding adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,10 @@
 import { defineConfig } from "astro/config";
-
 import tailwind from "@astrojs/tailwind";
+import netlify from "@astrojs/netlify/functions";
 
 // https://astro.build/config
 export default defineConfig({
   output: "hybrid",
   integrations: [tailwind()],
+  adaoter: netlify(),
 });
